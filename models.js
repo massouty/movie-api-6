5847ef002e873c908852980a801fa40ac184f326
massouty <massouty@outlook.com>
2022-10-27 15:38:04 +0200

    Update models.js
    if applied , this commit will have changed some codes

--- models.js
+++ models.js
@@ -1,4 +1,5 @@
 const mongoose = require ("mongoose");
+bcrypt = require('bcrypt'); // for hashing password
 let movieSchema = mongoose.Schema({Title:{type:String , required:true},
 Description:{type:String , required:true},
 Genre:{
@@ -13,15 +14,22 @@ ImagePath:String,
 Featured:Boolean
 });
 
-let userSchema = mongoose.Schema({userName:{type:String, required:true},
-password:{type:String , required:true},
-email:{type:String,required:true},
+let userSchema = mongoose.Schema({Username:{type:String, required:true},
+Password:{type:String , required:true},
+Email:{type:String,required:true},
 Birthday: Date,
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
+// hashing of submitted passwords
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);}
+
+
 let Movie = mongoose.model('Movie', movieSchema);
 let User = mongoose.model('User', userSchema);
+
+
 
 module.exports.Movie = Movie;
 module.exports.User = User;
